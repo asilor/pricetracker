75daf8ac34ab0337af47f79569b2de9184e21f33
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-23 00:19:24 +0100

    add mongodb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-  ]
+  ],
+
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
