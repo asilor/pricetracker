dab3b48fd88965464be2af51c61623fac6904530
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-28 19:08:12 +0100

    update product page route

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -5,12 +5,12 @@
 <div class="space-y-2">
     <span class="bg-red-500 text-white rounded-full px-2 text-sm z-10">-{product.discount}%</span>
     <div>
-        <a href={`/prices/${product._id}`}>
+        <a href={`/prices/${product._id}/${product.slug}`}>
             <img src={product.image} alt={product.title} class="hover:scale-[102%] transition" />
         </a>        
     </div>
     <h2 class="text-xl font-medium font-cabinet-grotesk hover:underline">
-        <a href={`/prices/${product._id}`}>{product.title}</a>
+        <a href={`/prices/${product._id}/${product.slug}`}>{product.title}</a>
     </h2>
     <div>{product.price}</div>
 </div>
