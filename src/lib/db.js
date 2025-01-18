b0554727bc85cb49e63cba6ad48a1f2ea303325f
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-19 00:28:06 +0100

    mongodb connection

---
+++ src/lib/db.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+const mongo_user = import.meta.env.VITE_MONGO_USER;
+const mongo_password = import.meta.env.VITE_MONGO_PASSWORD;
+const mongo_ip = import.meta.env.VITE_MONGO_IP;
+const mongo_port = import.meta.env.VITE_MONGO_PORT;
+const mongo_db_name = import.meta.env.VITE_MONGO_DB_NAME;
+
+const mongo_uri = `mongodb://${mongo_user}:${mongo_password}@${mongo_ip}:${mongo_port}`;
+
+let client = null;
+
+export async function connection() {
+  if (!client) {
+    client = new MongoClient(mongo_uri);
+    await client.connect();
+  }
+  return client.db(mongo_db_name);
+}
