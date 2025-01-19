c5d205f3ee7c8fa12c32930e5ba6886d675b9e66
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-19 13:40:28 +0100

    header searchbox

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,17 @@
-<header class="sticky top-0 z-50 flex items-center h-18 bg-white border-b border-neutral-200">
+<script>
+    import SearchBox from "$lib/components/SearchBox.svelte";
+</script>
+
+<header class="sticky top-0 bg-white/80 backdrop-blur-2xl border-b border-neutral-100 flex items-center h-18 z-50">
     <div class="container mx-auto flex items-center px-4 lg:px-8 space-x-12">
         <a href="/" class="flex items-center space-x-3">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380 380" class="h-6 w-6">
                 <path fill="currentColor" d="M0 63.333C0 28.3553 28.3553 0 63.333 0c34.978 0 63.334 28.3553 63.334 63.333v63.334H63.333C28.3553 126.667 0 98.311 0 63.333Zm253.333 63.334H126.667v126.666H63.334C28.3554 253.333 0 281.688 0 316.666 0 351.645 28.3553 380 63.333 380c34.978 0 63.334-28.355 63.334-63.333v-63.334h126.666v63.333c0 34.979 28.355 63.334 63.333 63.334C351.645 380 380 351.645 380 316.666c0-34.978-28.355-63.333-63.334-63.333h-63.333zm0 0h63.334c34.978 0 63.333-28.356 63.333-63.334C380 28.3553 351.645 0 316.666 0c-34.978 0-63.333 28.3554-63.333 63.334Z"/>
             </svg>
-            <span class="text-3xl font-medium">Price Tracker</span>
+            <span class="text-3xl font-medium font-cabinet-grotesk">Price Tracker</span>
         </a>
+        <nav class="flex flex-1 items-center justify-center">
+            <SearchBox />
+        </nav>
     </div>
 </header>
