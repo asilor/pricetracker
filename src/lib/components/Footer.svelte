80cc0a743019bf24298c7286e7ea51742fd27693
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-19 13:40:33 +0100

    footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,55 @@
+<footer class="border-t border-neutral-200">
+    <div class="container mx-auto px-4 lg:px-8 py-6 lg:py-12">
+        <div class="grid grid-cols-2 lg:grid-cols-5 gap-8 mb-8 lg:mb-24">
+            <div class="col-span-2 space-y-4">
+                <a href="/" class="flex items-center space-x-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380 380" class="h-6 w-6">
+                        <path fill="currentColor" d="M0 63.333C0 28.3553 28.3553 0 63.333 0c34.978 0 63.334 28.3553 63.334 63.333v63.334H63.333C28.3553 126.667 0 98.311 0 63.333Zm253.333 63.334H126.667v126.666H63.334C28.3554 253.333 0 281.688 0 316.666 0 351.645 28.3553 380 63.333 380c34.978 0 63.334-28.355 63.334-63.333v-63.334h126.666v63.333c0 34.979 28.355 63.334 63.333 63.334C351.645 380 380 351.645 380 316.666c0-34.978-28.355-63.333-63.334-63.333h-63.333zm0 0h63.334c34.978 0 63.333-28.356 63.333-63.334C380 28.3553 351.645 0 316.666 0c-34.978 0-63.333 28.3554-63.333 63.334Z"/>
+                    </svg>
+                    <span class="text-3xl font-medium font-cabinet-grotesk">Price Tracker</span>
+                </a>
+                <p class="text-sm text-neutral-600">Buy at the right place at the right time.</p>
+            </div>
+            <div class="col-span-1">
+                <h3 class="font-medium">About</h3>
+                <ul class="mt-4 space-y-2 text-sm">
+                    <li>
+                        <a href="/" class="hover:underline">Link</a>
+                    </li>
+                    <li>
+                        <a href="/" class="hover:underline">Link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-span-1">
+                <h3 class="font-medium">Resources</h3>
+                <ul class="mt-4 space-y-2 text-sm">
+                    <li>
+                        <a href="/" class="hover:underline">Link</a>
+                    </li>
+                    <li>
+                        <a href="/" class="hover:underline">Link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-span-1">
+                <h3 class="font-medium">Partners</h3>
+                <ul class="mt-4 space-y-2 text-sm">
+                    <li>
+                        <a href="/" class="hover:underline">Link</a>
+                    </li>
+                    <li>
+                        <a href="/" class="hover:underline">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="border-t border-neutral-200 py-4 lg:flex items-center justify-between space-y-4">
+            <span class="text-sm">Copyright Price Tracker. All rights reserved.</span>
+            <div class="flex items-center space-x-4 text-sm">
+                <a href="/terms-and-conditions" class="hover:underline">Terms and Conditions</a>
+                <a href="/privacy-policy" class="hover:underline">Privacy policy</a>
+            </div>
+        </div>
+    </div>
+</footer>
