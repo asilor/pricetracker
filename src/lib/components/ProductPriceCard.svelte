19d89eacdf8032192ef375547511133e37ec0938
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-21 22:27:52 +0100

    product price card

--- src/lib/components/ProductPriceCard.svelte
+++ src/lib/components/ProductPriceCard.svelte
@@ -1,7 +1,13 @@
-<li class="bg-white rounded px-4 py-2 flex items-center justify-between">
-    <img class="rounded-full w-12 h-12" src="https://assets.klarnacdn.net/rs:fit:200:200:1:1/bg:255:255:255/resize:fit:76:76/quality:80/format:avif/image/11bbe186-1e2b-4487-8f7a-eca565d679a7/44d4df7c629c606b2667e91d6c8c0f3b" alt="Amazon">
-    <span class="text-2xl">1299€</span>
-    <svg viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
-        <path d="M15 2v7.5h-1V3.832l-9.334 9.334-.707-.707L13.418 3H7.5V2H15zm-4 14H1V6h6.574V5H0v12h12V9.286h-1V16z"/>
-    </svg>
-</li>
+<script>
+    const id = "6767f66f21cc010007e2777c";
+</script>
+
+<a href={`/api/view-product/${id}`} rel="nofollow noopener noreferrer" target="_blank">
+    <li class="bg-white rounded-xl px-4 py-2 flex items-center justify-between border border-neutral-200">
+        <img class="rounded-full w-12 h-12" src="https://assets.klarnacdn.net/rs:fit:200:200:1:1/bg:255:255:255/resize:fit:76:76/quality:80/format:avif/image/11bbe186-1e2b-4487-8f7a-eca565d679a7/44d4df7c629c606b2667e91d6c8c0f3b" alt="Amazon">
+        <span class="text-2xl">1299€</span>
+        <svg viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
+            <path d="M15 2v7.5h-1V3.832l-9.334 9.334-.707-.707L13.418 3H7.5V2H15zm-4 14H1V6h6.574V5H0v12h12V9.286h-1V16z"/>
+        </svg>
+    </li>
+</a>
