d9727baab4af5f55229676e85c51585ef704f914
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-21 22:29:07 +0100

    update relocate url

--- src/lib/components/ProductPriceCard.svelte
+++ src/lib/components/ProductPriceCard.svelte
@@ -2,7 +2,7 @@
     const id = "6767f66f21cc010007e2777c";
 </script>
 
-<a href={`/api/view-product/${id}`} rel="nofollow noopener noreferrer" target="_blank">
+<a href={`/relocate/${id}`} rel="nofollow noopener noreferrer" target="_blank">
     <li class="bg-white rounded-xl px-4 py-2 flex items-center justify-between border border-neutral-200">
         <img class="rounded-full w-12 h-12" src="https://assets.klarnacdn.net/rs:fit:200:200:1:1/bg:255:255:255/resize:fit:76:76/quality:80/format:avif/image/11bbe186-1e2b-4487-8f7a-eca565d679a7/44d4df7c629c606b2667e91d6c8c0f3b" alt="Amazon">
         <span class="text-2xl">1299€</span>
