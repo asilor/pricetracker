9540e1c5d18030f4044c0fa82e0b8aa90a761842
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-26 16:19:45 +0100

    complete mongodb query join retailers

--- src/lib/components/RetailerPriceCard.svelte
+++ src/lib/components/RetailerPriceCard.svelte
@@ -4,8 +4,8 @@
 
 <a href={retailer.link} rel="nofollow noopener noreferrer" target="_blank">
     <li class="bg-white rounded-xl px-4 py-2 flex items-center justify-between border border-neutral-200">
-        <img class="w-40" src={retailer.retailer_logo_url} alt={retailer.retailer_name} />
-        <h1>{retailer.retailer_name}</h1>
+        <img class="w-36" src={retailer.logo_url} alt={retailer.retailer_name} />
+        <h1>{retailer.name}</h1>
         <span class="text-2xl">{retailer.price}€</span>
         <svg viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
             <path d="M15 2v7.5h-1V3.832l-9.334 9.334-.707-.707L13.418 3H7.5V2H15zm-4 14H1V6h6.574V5H0v12h12V9.286h-1V16z"/>
--- src/routes/compare-prices/[slug_id]/+page.server.js
+++ src/routes/compare-prices/[slug_id]/+page.server.js
@@ -4,42 +4,107 @@ import { ObjectId } from 'mongodb'
 export async function load({ params }) {
   const { slug_id } = params;
   const index = slug_id.lastIndexOf("-");  
-  const variant_id = "679617d221cc010007e27926" //slug_id.substring(index + 1);
-  const language = "es";
+  const variant_id = slug_id.substring(index + 1);
+  const language = "en";
   const region = "ES";
 
   const db = await connection();
   const variants_collection = db.collection('variants');
-  const products_collection = db.collection('products');
-
-  const variant = await variants_collection.findOne({ _id: new ObjectId(variant_id) },);
-  const product = await products_collection.findOne({ _id: new ObjectId(variant.product_id) },);
-
-  const result = {
-    // variant
-    variant_id: variant._id.toString(),
-    thumbnails: variant.thumbnails,
-    images: variant.images,
-    title: variant.languages[language].title,
-    slug: variant.languages[language].slug,
-    retailers: variant.regions[region].retailers.map(retailer => ({
-      retailer_id: retailer.retailer_id.toString(),
-      link: retailer.link,
-      shipping_cost: retailer.shipping_cost,
-      price: retailer.price
-    })),
-
-    // product
-    categories: product.categories,
-    options: product.options,
-    variants: product.variants.map(variant => ({
-      variant_id: variant.variant_id.toString(),
-      values: variant.values
-    })),
-    brand: product.brand,
-    ratings: product.ratings,
-    description: product.languages[language].description
-  };
+  
+  const [result] = await variants_collection.aggregate([
+    {
+      $match: {
+        _id: new ObjectId(variant_id)
+      }
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "product_id",
+        foreignField: "_id",
+        as: "product"
+      }
+    },
+    {
+      $unwind: "$product"
+    },
+    {
+      $project: {
+        _id: 1,
+        thumbnails: 1,
+        images: 1,
+        "languages": 1,
+        "regions": 1,
+        product: 1,
+        retailers: `$regions.${region}.retailers`
+      }
+    },
+    {
+      $unwind: "$retailers"
+    },
+    {
+      $lookup: {
+        from: "retailers",
+        localField: "retailers.retailer_id",
+        foreignField: "_id",
+        as: "retailer_info"
+      }
+    },
+    {
+      $unwind: "$retailer_info"
+    },
+    {
+      $group: {
+        _id: "$_id",
+        variant_id: { $first: "$_id" },
+        thumbnails: { $first: "$thumbnails" },
+        images: { $first: "$images" },
+        title: { $first: `$languages.${language}.title` },
+        slug: { $first: `$languages.${language}.slug` },
+        retailers: {
+          $push: {
+            name: "$retailer_info.name",
+            logo_url: "$retailer_info.logo_url",
+            link: "$retailers.link",
+            shipping_cost: "$retailers.shipping_cost",
+            price: "$retailers.price"
+          }
+        },
+        categories: { $first: "$product.categories" },
+        options: { $first: "$product.options" },
+        variants: { $first: "$product.variants" },
+        brand: { $first: "$product.brand" },
+        ratings: { $first: "$product.ratings" },
+        description: { $first: `$product.languages.${language}.description` }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        variant_id: { $toString: "$variant_id" },
+        thumbnails: 1,
+        images: 1,
+        title: 1,
+        slug: 1,
+        retailers: 1,
+        categories: 1,
+        options: 1,
+        variants: {
+          $map: {
+            input: "$variants",
+            as: "variant",
+            in: {
+              variant_id: { $toString: "$$variant.variant_id" },
+              values: "$$variant.values"
+            }
+          }
+        },
+        brand: 1,
+        ratings: 1,
+        description: 1
+      }
+    }
+  ]).toArray();
 
   return { result };
 }
