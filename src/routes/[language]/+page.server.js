12f706833fd75e770fed6624365f92a6fada45e7
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-27 00:41:53 +0100

    query first 10 products

--- src/routes/[language]/+page.server.js
+++ src/routes/[language]/+page.server.js
@@ -1,14 +1,40 @@
 import { connection } from '$lib/db';
+import { locale } from '$lib/state/locale.svelte';
 
-export async function load() {
+export async function load({ parent }) {
   const db = await connection();
-  const collection = db.collection('products');
-  const products = await collection.find().toArray();
+  const variants_collection = db.collection('variants');
 
-  const updatedProducts = products.map(product => ({
-    ...product,
-    _id: product._id.toString()
-  }));
+  await parent(); // update locale state first
 
-  return { products: updatedProducts };
+  const products = await variants_collection.aggregate([
+    { $limit: 10 },
+    // join variant with product
+    { $lookup: {
+      from: "products", 
+      localField: "product_id",
+      foreignField: "_id",
+      as: "product"
+    } },
+    { $addFields: {
+      matched_variant: {
+        $first: {
+          $filter: {
+            input: "$product.variants",
+            cond: { $eq: ["$$this.variant_id", "$_id"] }
+          }
+        }
+      }
+    } },
+    { $group: {
+      _id: { $toString: "$_id" },
+      title: { $first: `$languages.${locale.language}.title` },
+      images: { $first: "$images" },
+      slug: { $first: `$matched_variant.languages.${locale.language}.slug` }
+    } }
+  ]).toArray();
+
+  console.log(products);
+
+  return { products };
 }
